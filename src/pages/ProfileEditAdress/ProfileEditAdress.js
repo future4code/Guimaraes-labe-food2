7eb49988b46c932580a35e03dae97bdb01472679
Lucas Aversi <lucasaversi@gmail.com>
2022-05-10 21:52:53 -0300

    Estilização dos forms

--- src/pages/ProfileEditAdress/ProfileEditAdress.js
+++ src/pages/ProfileEditAdress/ProfileEditAdress.js
@@ -8,7 +8,9 @@ import { useNavigate } from 'react-router-dom';
 import homepageGray from '../../img/homepageGray.png'
 import Cart_gray from '../../img/shopping-cart_gray.svg'
 import avatarGreen from '../../img/avatarGreen.png'
+import back from '../../img/back.png'
 import {FooterProfile} from '../Profile/style'
+import { MainDiv, Form, Subtitle, BackImg, TitleDiv   } from './style';
 
 const ProfileEditAdress = () => {
 
@@ -39,11 +41,15 @@ const ProfileEditAdress = () => {
     });
   }
   return (
-    <div>
-      <p>Endereço</p>
-      <form onSubmit={submitForm}>
+    <MainDiv>
+      <TitleDiv>
+        <BackImg src={back} onClick={() => {goToProfile(navigate)}} />
+        <Subtitle>Endereço</Subtitle>
+      </TitleDiv>
 
-      <input
+      <Form onSubmit={submitForm}>
+
+      <input  
           value={form.street}
           placeholder="Rua, Av, etc"
           name="street"
@@ -106,12 +112,8 @@ const ProfileEditAdress = () => {
             SALVAR
           </button>
           
-        </form>
+        </Form>
 
-        <button
-          onClick={() => {goToProfile(navigate)}}>
-            Back
-        </button>
         <FooterProfile>
                 <div>
                     <img src={homepageGray} onClick={() => {goToHome(navigate)}} />
@@ -123,7 +125,7 @@ const ProfileEditAdress = () => {
                     <img src={avatarGreen} onClick={reload} />
                 </div>  
           </FooterProfile>
-    </div>
+    </MainDiv>
   )
 }
 
