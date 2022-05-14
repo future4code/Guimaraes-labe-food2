58a04ffe2393505c0fbedba28f46f8245c3397d5
AlexBitenc0urt <alexx.bittencourt@gmail.com>
2022-05-14 16:50:48 -0300

    add pagina registro

--- src/pages/Register/Register.js
+++ src/pages/Register/Register.js
@@ -1,14 +1,14 @@
-import React from "react";
-import useProtectedPage from "../../Hooks/UseProtectedPage";
-
+import React from "react"
+import { ScreenContainer, LogoImage } from "./style"
+import logo from '../../img/logo-future-eats-invert@2x.png'
+import RegisterForm from "./RegisterForm"
 
 export const Register = () => {
-    useProtectedPage();
-
     return(
-        <div>
-            Register
-        </div>
+        <ScreenContainer>
+            <LogoImage src={logo}/>
+            <RegisterForm/>
+        </ScreenContainer>
     )
 }
 
